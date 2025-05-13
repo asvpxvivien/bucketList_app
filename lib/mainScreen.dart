d7c69c38b5fb21bee9fa294da5b4f233d9228034
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-05-13 23:14:55 +0100

    installing packages for HTTP Requests

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -10,6 +10,6 @@ class Mainscreen extends StatefulWidget {
 class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(appBar: AppBar(title: Text("Bucket List")));
   }
 }
