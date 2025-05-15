8c87d2c754d3ee7436c8d7f2cef3cbaba557b360
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-05-15 23:21:33 +0100

    Loading state Indicator and RefreshIndicator

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -10,15 +10,22 @@ class Mainscreen extends StatefulWidget {
 
 class _MainscreenState extends State<Mainscreen> {
   List<dynamic> bucketlistData = [];
+  bool isLoading = false;
   Future<void> getData() async {
+    setState(() {
+      isLoading = true;
+    });
     try {
       Response response = await Dio().get(
         "https://flutterapitest123-e4e6a-default-rtdb.firebaseio.com/bucketlist.json",
       );
 
       bucketlistData = response.data;
+      isLoading = false;
       setState(() {});
     } catch (e) {
+      isLoading = false;
+      setState(() {});
       showDialog(
         context: context,
         builder: (context) {
@@ -31,35 +38,53 @@ class _MainscreenState extends State<Mainscreen> {
   }
 
   @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Bucket List")),
-      body: Column(
-        children: [
-          ElevatedButton(onPressed: getData, child: Text("Get data")),
-          Expanded(
-            child: ListView.builder(
-              itemCount: bucketlistData.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        bucketlistData[index]['image'] ?? "",
-                      ),
-                    ),
-                    title: Text(bucketlistData[index]['item'] ?? ""),
-                    trailing: Text(
-                      bucketlistData[index]['cost'].toString() ?? "",
-                    ),
-                  ),
-                );
-              },
+      appBar: AppBar(
+        title: Text("Bucket List"),
+        actions: [
+          InkWell(
+            onTap: getData,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.refresh),
             ),
           ),
         ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          getData();
+        },
+        child:
+            isLoading
+                ? Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                  itemCount: bucketlistData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: NetworkImage(
+                            bucketlistData[index]['image'] ?? "",
+                          ),
+                        ),
+                        title: Text(bucketlistData[index]['item'] ?? ""),
+                        trailing: Text(
+                          bucketlistData[index]['cost'].toString() ?? "",
+                        ),
+                      ),
+                    );
+                  },
+                ),
       ),
     );
   }
