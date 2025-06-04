2ea61b9db3d7a6d78a7f41eb5237ad2976c43b4a
Vivien Amagbegnon <amagbegnonvivien7@gmail.com>
2025-06-04 22:25:56 +0100

    Adding data to API

--- lib/add_BucketList.dart
+++ lib/add_BucketList.dart
@@ -8,8 +8,24 @@ class AddBucketListScreen extends StatefulWidget {
 }
 
 class _AddBucketListScreenState extends State<AddBucketListScreen> {
+  Future<void> markAsComplete() async {
+    try {
+      Map<String, dynamic> data = {"completed": true};
+      Response response = await Dio().patch(
+        "https://flutterapitest123-e4e6a-default-rtdb.firebaseio.com/bucketlist/${widget.index}.json",
+        data: data,
+      );
+      Navigator.pop(context, "refresh");
+    } catch (e) {
+      print("Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Add Bucket List")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Add Bucket List")),
+      body: ElevatedButton(onPressed: () {}, child: Text("Add data")),
+    );
   }
 }
